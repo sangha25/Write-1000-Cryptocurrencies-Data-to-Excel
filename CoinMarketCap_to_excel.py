8d73328dcaf3a227728bf35efb8dc90e0e6e2e08
Sanghapal Shelke <sanghapal25@gmail.com>
2018-11-11 12:37:27 +0530

    Update CoinMarketCap_to_excel.py

--- CoinMarketCap_to_excel.py
+++ CoinMarketCap_to_excel.py
@@ -1,3 +1,6 @@
 import requests
 from openpyxl import Workbook
+listings_api = 'https://api.coinmarketcap.com/v2/listings/'
+ticker_api = 'https://api.coinmarketcap.com/v2/ticker/?start='
+listings_data = requests.get(listings_api).json()['data']
 
