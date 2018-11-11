c0a94e5fffd69ef27a548a7ecf6120963b34baa6
Sanghapal Shelke <sanghapal25@gmail.com>
2018-11-11 12:37:47 +0530

    Update CoinMarketCap_to_excel.py

--- CoinMarketCap_to_excel.py
+++ CoinMarketCap_to_excel.py
@@ -4,3 +4,7 @@ listings_api = 'https://api.coinmarketcap.com/v2/listings/'
 ticker_api = 'https://api.coinmarketcap.com/v2/ticker/?start='
 listings_data = requests.get(listings_api).json()['data']
 
+file = Workbook()
+sheet = file.create_sheet('Sheet 1',0)
+sheet.append(['Name','Symbol','Price','Volume','MarketCap','Change 1h','Change 24h','Change 7d'])
+
